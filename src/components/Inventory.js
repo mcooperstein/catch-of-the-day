d6985392a423fae89a5a2fc050f3ecc2639e16e3
Marc Cooperstein <mcooperstein@gmail.com>
2018-04-11 18:35:14 -0700

    set up tri-fold menu structure and components

---
+++ src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React, {Component, Fragment} from 'react';
+
+class Inventory extends Component {
+  render(){
+    return(
+      <div className="inventory">Inventory</div>
+    )
+  }
+}
+
+export default Inventory;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import StorePicker from './components/StorePicker';
+import App from './components/App';
 import './css/style.css';
 
-class App extends Component {
+/*class App extends Component {
   render(){
     return (
       <StorePicker/>
     )
   }
-}
+}*/
 
 ReactDOM.render(<App />, document.getElementById('main'))
